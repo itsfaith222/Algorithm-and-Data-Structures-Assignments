79d0f6272613789add17f650b1431a77cee6638a
itsfaith222 <faithaikhionbare@outlook.com>
2025-02-07 00:21:17 +0000

    second changes line 15 and 16

--- Test.c
+++ Test.c
@@ -11,6 +11,8 @@ int main() {
     printf("The sum of %d and %d is %d\n", num1, num2, sum);
 
     printf("This is a test of adding code to the file");
+
+    printf("Second added line to test commit");
     
     return 0;
 }
