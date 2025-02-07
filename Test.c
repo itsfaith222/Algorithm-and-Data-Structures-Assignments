9b44e7210c5189522e8ef288cf44fae02ec36f48
itsfaith222 <faithaikhionbare@outlook.com>
2025-02-06 19:46:54 -0500

    Empty the test file

--- Test.c
+++ Test.c
@@ -1,22 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int num1, num2, sum;
     
-    printf("Hello, Faith!\n");
-    printf("Enter two numbers: ");
-    scanf("%d %d", &num1, &num2);
-    
-    sum = num1 + num2;
-    printf("The sum of %d and %d is %d\n", num1, num2, sum);
-
-    printf("This is a test of adding code to the file");
-    
-
-    printf("Another change");
-
-
-
 
 
     return 0;
